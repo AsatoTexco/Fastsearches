849dddce81c19fe9a052d86afefbd56f0886af20
AsatoTexco <arthur2006.teixeira@gmail.com>
2023-07-30 03:01:51 -0300

    finalizado conectando a um servidor

--- config.php
+++ config.php
@@ -1,12 +1,24 @@
 <?php
 
 
-$db_name = 'pesquisa';
-$db_host = 'gateway01.eu-central-1.prod.aws.tidbcloud.com';
-$db_user = 'EKd5Yr8hnJ9iKyz.root';
-$db_password = 'vNtOyy6KJHNQ0XGu';
+$db_name = 'nome_banco';
+$db_host = 'host';
+$db_user = 'user_banco';
+$port = 'porta';
+$db_password = 'senha_do_banco';
+$ssl_ca = '<path_to_ca_cert>';
 
-$pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host,$db_user,$db_password);
+// options para enviar de forma segura 
+$options = [
+    PDO::MYSQL_ATTR_SSL_CA => $ssl_ca,
+    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false, 
+];
+$pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host.":".$port,$db_user,$db_password,$options);
+
+$sql = $pdo->query('SELECT * from user');
+
+print_r($sql->fetchAll(PDO::FETCH_ASSOC));
+
 
 
 ?>
